6b9adf9bf72867b18673113e5f36f0dac6d37627
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-08-04 18:59:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
 
-//load calendar daily page events view
-$(document).ready(function(){
-    $("button").click(function(){
-      $("#create").load("   ");
-    });
-  });
+// //load calendar daily page events view
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $("#create").load("   ");
+//     });
+//   });
 
-  //when already in event view, load calendar weekly page fun facts view
-$(document).ready(function(){
-    $("button").click(function(){
-      $("#funFactsView").load("   ");
-    });
-  });
+//   //when already in event view, load calendar weekly page fun facts view
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $("#funFactsView").load("   ");
+//     });
+//   });
 
-//When in event view  
-//add event input to Monday's corresponding list element (or corresponding time block child of a day parent)
-  function addEvent() {
-    var x = document.getElementById("mondayEventsLog");
-    var option = document.createElement("eventBlock");
-    option.text = "   ";
-    x.add(option);
-  }
+// //When in event view  
+// //add event input to Monday's corresponding list element (or corresponding time block child of a day parent)
+//   function addEvent() {
+//     var x = document.getElementById("mondayEventsLog");
+//     var option = document.createElement("eventBlock");
+//     option.text = "   ";
+//     x.add(option);
+//   }
 
 //moment.js to get current time
-    moment('01/01/2016', 'MM/DD/YYYY', true).format()
+//moment('01/01/2016', 'MM/DD/YYYY', true).format();
+ //var dayDisplay = document.getElementById("#today");
+ //var now = moment().format("dddd MMMM Do, YYYY");
+  //dayDisplay=now;
+  var dayDisplay=moment().format("dddd, MMMM Do, YYYY");
+  $("#today").text(dayDisplay);
+ //console.log(now);
 
 
 
-//save api's current output to today's calendar funFact View
-    function addEvent() {
-    var x = document.getElementById("mondayFunFactsLog");
-    var option = document.createElement("eventBlock");
-    option.text = "   ";
-    x.add(option);
-  }
+// //save api's current output to today's calendar funFact View
+//     function addEvent() {
+//     var x = document.getElementById("mondayFunFactsLog");
+//     var option = document.createElement("eventBlock");
+//     option.text = "   ";
+//     x.add(option);
+//   }
 
 //hide all elements specific to events view, and show all specific to funFacts(and vice-versa),
 //when toggling from one of the Weekly Views to the other
-$(document).ready(function(){
-    $("#eventsView").click(function(){
-      $("  an events div ").hide();
-    });
-    $("#funFactsView").click(function(){
-      $(" a funFacts div").show();
-    });
-  });
+// $(document).ready(function(){
+//     $("#eventsView").click(function(){
+//       $("  an events div ").hide();
+//     });
+  //   $("#funFactsView").click(function(){
+  //     $(" a funFacts div").show();
+  //   });
+  // });
 
-    //reverse, for the "to Events Views" button, hide all funFact cal view and show Event cal view
-    $(document).ready(function(){
-        $("#eventsView").click(function(){
-          $("  an events div ").hide();
-        });
-        $("#funFactsView").click(function(){
-          $(" a funFacts div").show();
-        });
-      });
-    
+  //   //reverse, for the "to Events Views" button, hide all funFact cal view and show Event cal view
+  //   $(document).ready(function(){
+  //       $("#eventsView").click(function(){
+  //         $("  an events div ").hide();
+  //       });
+  //       $("#funFactsView").click(function(){
+  //         $(" a funFacts div").show();
+  //       });
+  //     });
+  
+
+  //     //move the bellow code to js when available
+  //     $(function(){
+  //       $("#calendar").load("calIndex.html"); 
+  //     });
+      
